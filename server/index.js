c0fb48dcfa49f88935c821cfd36e8c46e229d46a
yourneme <mr.twister99@gmail.com>
2026-07-20 08:26:06 +0200

    Prepare server for Railway

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
